48d86470b6e441aea945b0e6c19e4184b03bccd1
Rakhmatdzhon Abdurashidov <rabduras@student.42.us.org>
2020-01-30 10:04:29 -0800

    added length of longest substring

--- add_two_numbers.c
+++ add_two_numbers.c
@@ -1,42 +1,42 @@
 #include <stdlib.h>
 
 struct ListNode {
-    int val;
-    struct ListNode *next;
+	int val;
+	struct ListNode *next;
 };
 
 struct ListNode *createNode(int val)
 {
-    struct ListNode *node;
-    
-    node = (struct ListNode*)malloc(sizeof(struct ListNode));
-    if (node != NULL)
-    {
-        node->val = val;
-        node->next = NULL;
-        return (node);
-    }
-    return (NULL);
+	struct ListNode *node;
+
+	node = (struct ListNode*)malloc(sizeof(struct ListNode));
+	if (node != NULL)
+	{
+		node->val = val;
+		node->next = NULL;
+		return (node);
+	}
+	return (NULL);
 }
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 {
-    int sum = 0;
-    struct ListNode *head = NULL;
-    struct ListNode **node = &head;
+	int sum = 0;
+	struct ListNode *head = NULL;
+	struct ListNode **node = &head;
 
-    while (l1 || l2 || sum) {
-        if (l1) {
-            sum += l1->val;
-            l1 = l1->next;
-        }
-        if (l2) {
-            sum += l2->val;
-            l2 = l2->next;
-        }
-        *node = createNode(sum % 10);
-        node = &((*node)->next);
-        sum /= 10;
-    }
-    return (head);
+	while (l1 || l2 || sum) {
+		if (l1) {
+			sum += l1->val;
+			l1 = l1->next;
+		}
+		if (l2) {
+			sum += l2->val;
+			l2 = l2->next;
+		}
+		*node = createNode(sum % 10);
+		node = &((*node)->next);
+		sum /= 10;
+	}
+	return (head);
 }
---
+++ length_of_longest_substring.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char * s){
+	int arr[256];
+	int i = 0;
+	int max = 0;
+	int start = -1;
+
+	memset((void*)arr, -1, 256*sizeof(int));
+	while(s[i])
+	{
+		if (arr[s[i]] > -1)
+			start = arr[s[i]];
+		arr[s[i]] = i;
+		max = max > i - start ? max : i - start;
+		i++;
+	}
+	return max;
+}
+
+void test(int actual, int expected)
+{
+	if (actual != expected)
+		printf("Test failed: expected %d, got %d", expected, actual);
+	else
+		printf("Success: expected %d, got %d", expected, actual);
+	printf("\n");
+}
+
+int main()
+{
+	char str1[] = "cdd";
+	char str2[] = "bbbbb";
+
+	test(lengthOfLongestSubstring(str1), 3);
+	test(lengthOfLongestSubstring(str2), 1);
+}
